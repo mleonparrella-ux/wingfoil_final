f03a091b0a7bec3fd8f8e0e1c7f7a85efd0b4010
agusfrey <afrey@udesa.edu.ar>
2026-06-03 15:12:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.recomendacion import recomendar_equipo
 from src.graficos import (grafico_sensacion_por_wing, grafico_sensacion_por_ubicacion,
                           grafico_viento_vs_sensacion)
 
-RUTA = "datos/sesiones_kite.xlsx"
+RUTA = "sesiones_kite.xlsx"
 
 
 def pedir_ubicacion():
