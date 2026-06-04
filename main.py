3a8465391a85e215186b665974529958c2ac82c4
agusfrey <afrey@udesa.edu.ar>
2026-06-04 15:21:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  4 13:39:05 2026
-
-@author: usuario
-"""
-
 from src.carga_datos import cargar_datos
 from src.recomendador import recomendar_wing
 from graficos.graficos import grafico_viento
@@ -12,52 +5,38 @@ from graficos.graficos import grafico_wings
 
 df = cargar_datos()
 
-while True:
+if df is None:
+    print("No se pudo cargar el archivo. El programa se detiene.")
+else:
+    while True:
+        print("\nWINGFOIL")
+        print("1 - Ver cantidad de sesiones")
+        print("2 - Recomendar wing")
+        print("3 - Gráfico viento")
+        print("4 - Gráfico wings")
+        print("0 - Salir")
+        opcion = input("Opción: ")
 
-    print("\nWINGFOIL")
-    print("1 - Ver cantidad de sesiones")
-    print("2 - Recomendar wing")
-    print("3 - Gráfico viento")
-    print("4 - Gráfico wings")
-    print("0 - Salir")
+        if opcion == "1":
+            print("\nTotal sesiones:", len(df))
 
-    opcion = input("Opción: ")
+        elif opcion == "2":
+            try:
+                viento    = float(input("Velocidad del viento (nudos): "))
+                direccion = input("Dirección del viento (ej: Norte, Noreste): ")
+                ubicacion = input("Ubicación (ej: Rio de la Plata): ")
+                recomendar_wing(df, viento, direccion, ubicacion)
+            except ValueError as e:
+                print(f"Error: {e}")
 
-    if opcion == "1":
+        elif opcion == "3":
+            grafico_viento(df)
 
-        print(
-            "\nTotal sesiones:",
-            len(df)
-        )
+        elif opcion == "4":
+            grafico_wings(df)
 
-    elif opcion == "2":
+        elif opcion == "0":
+            break
 
-        try:
-
-            viento = float(
-                input("Velocidad del viento: ")
-            )
-
-            recomendar_wing(
-                df,
-                viento
-            )
-
-        except:
-            print("Ingrese un número")
-
-    elif opcion == "3":
-
-        grafico_viento(df)
-
-    elif opcion == "4":
-
-        grafico_wings(df)
-
-    elif opcion == "0":
-
-        break
-
-    else:
-
-        print("Opción inválida")
+        else:
+            print("Opción inválida")
