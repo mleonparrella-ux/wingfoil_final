8579c6e81e1a98b628346ad1ed2e719fd5bb8e85
jkleinlarroude-tech <jkleinlarroude@udesa.edu.ar>
2026-06-12 13:48:57 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -40,3 +40,58 @@ else:
 
         else:
             print("Opción inválida")
+            
+from src.carga_datos import cargar_datos
+from graficos.graficos import grafico_wings
+from graficos.graficos import grafico_viento
+
+df = cargar_datos()
+
+print("\n====================")
+print(" DASHBOARD WINGFOIL ")
+print("====================")
+
+print("\nRESUMEN GENERAL")
+
+print("Total sesiones:", len(df))
+
+print(
+    "Viento promedio:",
+    round(df["Vel. Viento (kn)"].mean(),2)
+)
+
+print(
+    "Sensación promedio:",
+    round(df["Sensación"].mean(),2)
+)
+
+print(
+    "Cantidad de wings:",
+    df["Wing"].nunique()
+)
+
+while True:
+
+    print("\n--- DASHBOARD ---")
+
+    print("1 - Uso de Wings")
+    print("2 - Viento")
+    print("0 - Salir")
+
+    opcion = input("Seleccione: ")
+
+    if opcion == "1":
+
+        grafico_wings(df)
+
+    elif opcion == "2":
+
+        grafico_viento(df)
+
+    elif opcion == "0":
+
+        break
+
+    else:
+
+        print("Opción inválida")
